803580483e4477be2c91813368ae600796487aca
Elias Gill <eliasgill42@gmail.com>
2024-02-05 17:55:35 -0300

    initial commit

---
+++ elpa/cape-20240204.2213/cape-pkg.el
@@ -0,0 +1,15 @@
+(define-package "cape" "20240204.2213" "Completion At Point Extensions"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "d8e114a057ea53d8d91bbd9a6862bf76b3f02e37" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("abbrev" "convenience" "matching" "completion" "text")
+  :url "https://github.com/minad/cape")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
