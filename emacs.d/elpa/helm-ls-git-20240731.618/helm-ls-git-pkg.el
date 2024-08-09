0777f8d9c414513a776a17e43c7adf8ac5357f57
Elias Gill <eliasgill42@gmail.com>
2024-08-09 12:40:32 -0400

    feat: tsoding config

---
+++ emacs.d/elpa/helm-ls-git-20240731.618/helm-ls-git-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-ls-git" "20240731.618" "list git files."
+  '((helm "3.9.5")
+    (emacs "25.3"))
+  :commit "f92a7e2a11cc7d10243ca84d4fd80e32ace9a41d" :url "https://github.com/emacs-helm/helm-ls-git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
