ca6994cbcebf368d040c7763b8f5c7bec6fc0fb3
Elias Gill <eliasgill42@gmail.com>
2024-08-09 13:16:27 -0400

    fix: tsoding config

---
+++ elpa/racket-mode-20240808.1330/racket-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "racket-mode" "20240808.1330" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "f8485220dad158f78776d4e430f558a3097151b8" :authors
+  '(("Greg Hendershott" . "racket-mode-author@greghendershott.com"))
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
